22e5d942b553e9dc03e526d829e3d708be38d86f
Morgan Currie <morgan.c@getjobber.com>
2022-10-17 14:15:37 -0300

    Updates

---
+++ precache-manifest.56af10e73939364e51e782742f86b91a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5661070568377195b395",
+    "url": "/CanYouHearMeNow-Encrypted/static/css/main.2bd126d6.chunk.css"
+  },
+  {
+    "revision": "5661070568377195b395",
+    "url": "/CanYouHearMeNow-Encrypted/static/js/main.56610705.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/CanYouHearMeNow-Encrypted/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/CanYouHearMeNow-Encrypted/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "f2b3e55fd880af381e67",
+    "url": "/CanYouHearMeNow-Encrypted/static/js/runtime~main.f2b3e55f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/CanYouHearMeNow-Encrypted/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ba15c52e5d9c69aad2f69c8eac3e9cca",
+    "url": "/CanYouHearMeNow-Encrypted/index.html"
+  }
+];
